e82676ab7279a40d6292a588edbc84d27fd63dfb
nandayonah <fadlihdytullah@outlook.com>
2022-06-07 23:08:57 +0700

    Arrays

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { GoCommentDiscussion, GoThumbsup } from 'react-icons/go';
+
 import './style.css';
 
 const title = 'Hacker Stories';
@@ -12,6 +14,25 @@ const profile = {
   ],
   postsCount: 4,
 };
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 const getUppercaseWord = (word) => word.toUpperCase();
 
@@ -35,12 +56,45 @@ export default function App() {
         <input className="border-2 px-1" id="search" type="text" />
       </div>
 
-      <h2 className="text-2xl font-medium">Recently Posts</h2>
-      <ul>
-        {profile.posts.map((post, idx) => (
-          <li className="shadow p-2 mb-2" key={idx}>{post}</li>
-        ))}
-      </ul>
+      <div className="mb-3">
+        <h2 className="text-2xl font-medium">Recently Posts</h2>
+        <ul>
+          {profile.posts.map((post, idx) => (
+            <li className="shadow p-2 mb-2" key={idx}>
+              {post}
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="mb-3">
+        <h2 className="text-2xl font-medium">Recently Stories</h2>
+        <ul>
+          {list.map((item) => (
+            <li
+              className="shadow p-2 mb-2 flex justify-between"
+              key={item.objectID}
+            >
+              <div>
+                <span>
+                  <a href={item.url}>{item.title}</a>
+                </span>
+                <span className="text-sm text-gray-400 block">
+                  {item.author}
+                </span>
+              </div>
+              <div className="flex gap-2">
+                <div className="flex items-center gap-1 text-sm">
+                  <GoCommentDiscussion /> {item.num_comments}
+                </div>
+                <div className="flex items-center gap-1 text-sm">
+                  <GoThumbsup /> {item.points}
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
