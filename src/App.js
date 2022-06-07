50b8aaa1f56f3f738973ca77a6f265a78e2c1c39
nandayonah <fadlihdytullah@outlook.com>
2022-06-07 21:58:10 +0700

    3 files modified

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./style.css";
 
+const title = 'Habits Tracker'
+
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
+      <h1 className='text-4xl font-medium'>{title}</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
   );
